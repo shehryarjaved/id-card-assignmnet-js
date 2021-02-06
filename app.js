ce9b508e637a84c05e7c0bc62b05c3dda73d66f8
shehryarjaved <shehryarjaved007@gmail.com>
2021-02-07 01:46:09 +0500

    id card on js done

---
+++ app.js
@@ -0,0 +1,23 @@
+
+function myFunction() {
+    
+  
+
+
+
+    var Stdname = document.getElementById('name').value ;
+    var StdDesign = document.getElementById('Designation').value;
+    var StdDob = document.getElementById('dob').value;
+    var StdMNo =document.getElementById('M.no').value;
+    var StdAddress = document.getElementById('Address').value;
+    
+    document.getElementById("demo").innerHTML = Stdname;
+    document.getElementById("demo2").innerHTML = StdDesign;
+    document.getElementById("demo3").innerHTML = StdDob;
+    document.getElementById("demo4").innerHTML = StdMNo;
+    document.getElementById("demo5").innerHTML = StdAddress;
+    
+
+
+
+}
